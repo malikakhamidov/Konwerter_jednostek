f251dbf64eb066fcb0e92302044af05f0d02d0f4
Marcin Lipowski <1marcin.lipowski@gmail.com>
2026-04-07 20:25:09 +0200

    ci: fix flake8 errors

--- converter/temperature.py
+++ converter/temperature.py
@@ -1,6 +1,6 @@
-def c_to_f(v):   return (v * 9/5) + 32
-def c_to_k(v):   return v + 273.15
-def f_to_c(v):   return (v - 32) * 5/9
-def f_to_k(v):   return (v - 32) * 5/9 + 273.15
-def k_to_c(v):   return v - 273.15
-def k_to_f(v):   return (v - 273.15) * 9/5 + 32
+def c_to_f(v): return (v * 9/5) + 32
+def c_to_k(v): return v + 273.15
+def f_to_c(v): return (v - 32) * 5/9
+def f_to_k(v): return (v - 32) * 5/9 + 273.15
+def k_to_c(v): return v - 273.15
+def k_to_f(v): return (v - 273.15) * 9/5 + 32
