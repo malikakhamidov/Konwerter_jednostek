63fcd1549f66b0eb91c120eb8d45072fa76c2785
Marcin Lipowski <1marcin.lipowski@gmail.com>
2026-04-14 19:39:14 +0200

    Added Doc

--- converter/length.py
+++ converter/length.py
@@ -1,38 +1,48 @@
+#Centimeters to Meters
 def cm_to_m(v):
     return v / 100
 
 
+#Meters to Centimeters
 def m_to_cm(v):
     return v * 100
 
 
+#Centimeters to Inches
 def cm_to_inch(v):
     return v / 2.54
 
 
+#Inches to Centimeters
 def inch_to_cm(v):
     return v * 2.54
 
 
+#Centimeters to Kilometers
 def cm_to_km(v):
     return v / 100000
 
 
+#Kilometers to Centimeters
 def km_to_cm(v):
     return v * 100000
 
 
+#Centimeters to Feet
 def cm_to_feet(v):
     return v / 30.48
 
 
+#Feet to Centimeters
 def feet_to_cm(v):
     return v * 30.48
 
 
+#Kilometers to Miles
 def km_to_mile(v):
     return v / 1.609
 
 
+#Miles to Kilometers
 def mile_to_km(v):
     return v * 1.609
--- converter/mass.py
+++ converter/mass.py
@@ -1,38 +1,48 @@
+#Grams to Kilograms
 def g_to_kg(v):
     return v / 1000
 
 
+#Kilograms to Grams
 def kg_to_g(v):
     return v * 1000
 
 
+#Kilograms to Tonnes
 def kg_to_t(v):
     return v / 1000
 
 
+#Tonnes to Kilograms
 def t_to_kg(v):
     return v * 1000
 
 
+#Miligrams to Kilograms
 def mg_to_kg(v):
     return v / 1_000_000
 
 
+#Kilograms to Miligrams
 def kg_to_mg(v):
     return v * 1_000_000
 
 
+#Miligrams to Grams
 def mg_to_g(v):
     return v / 1000
 
 
+#Grams to Miligrams
 def g_to_mg(v):
     return v * 1000
 
 
+#Kilograms to Pounds
 def kg_to_lb(v):
     return v * 2.20462
 
 
+#Pounds to Kilograms
 def lb_to_kg(v):
     return v / 2.20462
